624b985ad9a2c3ee1291dc294333609937d41ff8
Anthony <anthony.amoz.s.a@gmail.com>
2023-02-08 09:45:02 -0300

    Criacao dos relacionamentos das models de contato

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -11,16 +11,13 @@ class Contato extends Model{
      */
     protected $table = 'contatos';
 
-
     /**
      * The attributes that  should be hidden for arrays
     * @var array
     */
     protected $hidden = [
 
-
     ];
-
 
     /**
      * The accessors to append to the model's arrays form
@@ -28,7 +25,18 @@ class Contato extends Model{
     */
     protected $appends = [
 
-
     ];
 
+
+    public function enderecoRelationship(){
+        return $this->belongsTo(Endereco::class, 'endereco_id');
+    }
+
+    public function telefoneRelationship(){
+        return $this->hasMany(Telefone::class, 'contato_id');
+    }
+
+    public function categoriaRelationship(){
+        return $this->belongsToMany(Categoria::class, 'contatos_has_categorias', 'contato_id', 'categoria_id');
+    }
 }
