33c43d887ca0c6b202073fe5873c791f1536e606
Anthony <anthony.amoz.s.a@gmail.com>
2023-02-10 09:57:54 -0300

    Criação do edit no controller

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -113,8 +113,7 @@ class ContatoController extends Controller{
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id){
         //
     }
 
